9ba04eb3f1068768d267c042f7300fc8746ffd93
sun wei <1793786487@qq.com>
2020-02-06 12:35:50 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//app.js
+App({
+  onLaunch: function () {
+ 
+
+    // 登录
+    wx.login({
+      success: res => {
+        console.log(res)
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  
+  },
+  
+})
